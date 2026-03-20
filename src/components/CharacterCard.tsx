9190ccc5302411d61549b0ae3ac15f3900835615
alnrfLO <raf.atns1103@gmail.com>
2026-03-20 03:19:01 +0100

    feat: page personnages avec grid RPG

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 function CharacterCard ({personnage} : Props) {
     return (
-        <div>
-            <h3>{personnage.nom}</h3>
+        <div className='card'>
             <img src={personnage.image} alt={personnage.nom} />
+            <h3>{personnage.nom}</h3>
             <span>{personnage.estVampire ? "🧛 Vampire" : "👤 Humain"}</span>
         </div>
     )
