3f2d979ee3ab2fde8d2914f957e97e4db2189365
alnrfLO <raf.atns1103@gmail.com>
2026-04-04 20:12:59 +0200

    les perso en diagonal

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -2,21 +2,38 @@ import NavBar from "../components/Navbar"
 import StarBackground from "../components/StarBackground"
 import { characters, vampires } from "../data/characters"
 import './Characters.css'
-import CharacterCard from "../components/CharacterCard"
+import { useState } from 'react'
 
 
 const toutLesPersonnage  = [...characters, ...vampires]
 
 export default function Characters() {
+  const [selected, setSelected] = useState(toutLesPersonnage[0])
   return (
-    <div className="hero" style={{ position: "relative" }}>
-      <NavBar />
-      <StarBackground />
-    <div className="characters-grid">
+  <div style={{ position: "relative" }}>
+    <NavBar />
+    <StarBackground />
+    <div className="characters-page">
+      <div className="diagonal-section">
       {toutLesPersonnage.map((personnage) => (
-        <CharacterCard key={personnage.nom} personnage={personnage} />
+        <div
+          key={personnage.nom}
+          className={personnage.nom === selected.nom ? "cadre actif" : "cadre flou"}
+          onClick={() => setSelected(personnage)}
+        >
+          <img src={personnage.image} alt={personnage.nom} />
+        </div>
       ))}
+      </div>
+      <div className="info-section">
+        <h2>{selected.nom}</h2>
+        <p>{selected.descriptions}</p>
+        <p>Age : {selected.age}</p>
+        <p>Objectif : {selected.objectif}</p>
+        <p>Personnalité : {selected.personnalite.join(', ')}</p>
+        <span>{selected.estVampire ? "🧛 Vampire" : "👤 Humain"}</span>
+      </div>
     </div>
-    </div>
+  </div>
   )
 }
